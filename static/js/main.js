7d6ceb1ee05a897e02e5838309c31e4eb7b25725
Dave Saksa <goallinorfold@gmail.com>
2016-09-26 14:30:09 -0400

    Removed chat -- added in the Zopim

--- static/js/main.js
+++ static/js/main.js
@@ -119,25 +119,6 @@ $(document).ready(function(){
         });
     });
 
-
-    $('#live-chat header').on('click', function() {
-
-		$('.chat').slideToggle(300, 'swing');
-		$('.chat-message-counter').fadeToggle(300, 'swing');
-
-	});
-
-	$("#cashier").on('click', function(e){
-	    $('#live-chat').fadeIn();
-	});
-
-	$('.chat-close').on('click', function(e) {
-
-		e.preventDefault();
-		$('#live-chat').fadeOut(300);
-
-	});
-
 });
 
 
